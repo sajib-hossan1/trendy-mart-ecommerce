924cf03301fe6f783f046d0e53634b4df649cf87
sajib-hossan1 <sajib.hossan112@gmail.com>
2021-09-13 01:38:23 +0600

    first three bug fixes

--- js/app.js
+++ js/app.js
@@ -8,20 +8,20 @@ loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
-  const allProducts = products.map((pd) => pd);
-  for (const product of allProducts) {
+  for (const product of products) {
+    console.log(product);
     const image = product.images;
     const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+    div.classList.add("single-product");
+    div.innerHTML = `
+    <div>
+      <img class="product-image" src=${product.image}></img>
+    </div>
+    <h3>${product.title}</h3>
+    <p>Category: ${product.category}</p>
+    <h2>Price: $ ${product.price}</h2>
+    <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+    <button id="details-btn" class="btn btn-danger">Details</button>
       `;
     document.getElementById("all-products").appendChild(div);
   }
